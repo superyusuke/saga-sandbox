e6c57ef96045956a687c5a3351afc4c3479e843a
Yusuke, Nakanishi <nakanishiyusuke0000@gmail.com>
2017-12-31 21:48:10 +0900

    s

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import sagaMonitor from './sagaMonitor';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import sagaMonitor from "./sagaMonitor";
 
-import Counter from './components/Counter';
-import reducer from './reducers';
-import rootSaga from './sagas';
+import Counter from "./components/Counter";
+import reducer from "./reducers";
+import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
@@ -18,13 +18,13 @@ function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')}
-      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')}
-      onIncrementAsync={() => action('INCREMENT_ASYNC')}
-      onAxios={() => action('DO_FETCH')}
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      onIncrementIfOdd={() => action("INCREMENT_IF_ODD")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
+      onAxios={() => action("DO_FETCH")}
     />,
-    document.getElementById('root'),
+    document.getElementById("root")
   );
 }
 
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,33 +1,29 @@
-import { delay } from 'redux-saga'
-import { put, call, takeEvery, all } from 'redux-saga/effects'
-import fetch from 'cross-fetch'
+import { delay } from "redux-saga";
+import { put, call, takeEvery, all } from "redux-saga/effects";
+import fetch from "cross-fetch";
 
-function * incrementAsync () {
-  yield delay(1000)
-  yield put({type: 'INCREMENT'})
+function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: "INCREMENT" });
 }
 
-function * doFetch () {
-  try{
-    const data = yield call(fetch, 'https://www.reddit.com/r/politics.json')
-    yield put({type: 'FETCH_SUCCEEDED', data})
-  }catch(error){
-    yield put({type: 'FETCH_ERROR', error})
+function* doFetch() {
+  try {
+    const data = yield call(fetch, "https://www.reddit.com/r/politics.json");
+    yield put({ type: "FETCH_SUCCEEDED", data });
+  } catch (error) {
+    yield put({ type: "FETCH_ERROR", error });
   }
-
 }
 
-function * watchIncrementAsync () {
-  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+function* watchIncrementAsync() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
 }
 
-function * watchFetch () {
-  yield takeEvery('DO_FETCH', doFetch)
+function* watchFetch() {
+  yield takeEvery("DO_FETCH", doFetch);
 }
 
-export default function * rootSaga () {
-  yield all([
-    watchIncrementAsync(),
-    watchFetch(),
-  ])
+export default function* rootSaga() {
+  yield all([watchIncrementAsync(), watchFetch()]);
 }
